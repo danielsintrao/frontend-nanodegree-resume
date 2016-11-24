6595da874e729fb7d53370eba214c7093c1ba747
Daniel Sintrao <danielsintrao@mediaweb.pt>
2016-11-24 10:06:34 +0000

    Append and Prepend Exercises

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,26 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
 
 
- $("#main").append("Daniel Sintrão");
+ //$("#main").append("Daniel Sintrão");
+
+ var awesomeToughts = "I am Daniel and I'm AWESOME";
+
+ 	console.log(awesomeToughts);
+
+ var funThoughts = awesomeToughts.replace("AWESOME" , "FUN");
+
+ 	//console.log(awesomeToughts);
+ 	//console.log(funThoughts);
+
+ 	$('#main').append(funThoughts);
+
+//----------------------- PART 2 ----------------------------- //
+
+var name = "Daniel Sintrão";
+var role = "Front End Developer";
+
+var formattedName = HTMLheaderName.replace("%data%",name);
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+
+	$('#header').prepend(formattedRole);
+	$('#header').prepend(formattedName);
+
